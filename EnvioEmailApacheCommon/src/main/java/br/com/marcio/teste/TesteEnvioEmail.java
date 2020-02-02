a29a1cd2993d08d4f9ce470f4981670555464a1f
marciofujikura <marciofujikura@gmail.com>
2020-02-02 12:24:38 -0300

    Update TesteEnvioEmail.java

--- EnvioEmailApacheCommon/src/main/java/br/com/marcio/teste/TesteEnvioEmail.java
+++ EnvioEmailApacheCommon/src/main/java/br/com/marcio/teste/TesteEnvioEmail.java
@@ -7,6 +7,6 @@ public class TesteEnvioEmail {
     public static void main(String[] args) {
 
         EmailJava emailJava = new EmailJava();
-        emailJava.enviarEmail("marciofujikura@hotmail.com", "E-mail de teste", "Foi enviado um novo e-mail de teste");
+        emailJava.enviarEmail("seuemail@hotmail.com", "E-mail de teste", "Foi enviado um novo e-mail de teste");
     }
 }
